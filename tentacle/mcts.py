e86edc74ade7ed196c29130ca8ea9060b52066b2
splendor <splendor.kill@gmail.com>
2016-06-11 22:40:59 +0800

    mcts + nn

--- tentacle/mcts.py
+++ tentacle/mcts.py
@@ -1,17 +1,17 @@
-from math import log, sqrt
-from random import choice
 import time
 
+from pybrain.datasets.supervised import SupervisedDataSet
+from pybrain.structure.modules.sigmoidlayer import SigmoidLayer
+from pybrain.supervised.trainers.backprop import BackpropTrainer
+from pybrain.tools.shortcuts import buildNetwork
+
+import numpy as np
 from tentacle.board import Board
 from tentacle.game import Game
 
 
 class MonteCarlo(object):
     def __init__(self, **kwargs):
-
-        self.states = []
-        self.wins = {}
-        self.plays = {}
 
         self.max_depth = 0
         self.stats = {}
@@ -22,145 +22,88 @@ class MonteCarlo(object):
         # Exploration constant, increase for more exploratory moves,
         # decrease to prefer moves with known higher win rates.
         self.C = float(kwargs.get('C', 1.4))
-        
-    
+
+        self.features_num = Board.BOARD_SIZE ** 2 * 3 + 2
+        self.hidden_neurons_num = self.features_num * 2
+        self.net = buildNetwork(self.features_num, self.hidden_neurons_num, 2, bias=True, outclass=SigmoidLayer)
+        self.trainer = BackpropTrainer(self.net)
+
+        self.total_sim = 0
+
+
     def select(self, board, moves, who, **kwargs):
-#         the_game = kwargs.get('context')
+        # Bail out early if there is no real choice to be made.
+        if not moves:
+            return
+        if len(moves) == 1:
+            return moves[0]
 
         if Game.on_training:
             self.calculation_time = 60
         else:
             self.calculation_time = 0.5
-        
+
         self.max_depth = 0
         self.stats = {}
-
-#         state = self.states[-1]
-        player = who  # board.current_player(state)
-        legal = moves  # board.legal_plays(self.states[:])
-
-        # Bail out early if there is no real choice to be made.
-        if not legal:
-            return
-        if len(legal) == 1:
-            return legal[0]
-
         games = 0
         begin = time.time()
-        
         while time.time() - begin < self.calculation_time:
-            self.sim(board, player)
+            self.sim(board)
             games += 1
 #             if games > 0:
 #                 break
-        
-#         print('states size: ', len(self.states))
-        
+
         self.stats.update(games=games, max_depth=self.max_depth, time=str(time.time() - begin))
-        print(len(self.plays), self.stats['games'], self.stats['time'])
-#         print("Maximum depth searched:", self.max_depth)
+        print(self.stats['games'], self.stats['max_depth'], self.stats['time'])
 
-        moves_states = moves  # [(p, board.next_state(state, p)) for p in legal]
-
-        # Display the stats for each possible play.
-        self.stats['moves'] = sorted(
-            ({'move': divmod(Board.change(board, S), Board.BOARD_SIZE),
-              'percent': 100 * self.wins.get((player, str(S)), 0) / self.plays.get((player, str(S)), 1),
-              'wins': self.wins.get((player, str(S)), 0),
-              'plays': self.plays.get((player, str(S)), 0)}
-             for S in moves_states),
-            key=lambda x: (x['percent'], x['plays']),
-            reverse=True
-        )
-        
-#         print('moves')
-#         for m in moves_states:
-#             print(m)
-#         print()
-#         print('wins')
-#         for m, c in self.wins:
-#             print(m, c)
-        
-            
-        
-#         for m in self.stats['moves']:
-#             print("{move}: {percent:.2f}% ({wins} / {plays})".format(**m))
-
-
-        
-        # Pick the move with the highest percentage of wins.
-#         percent_wins, num_moves, move = max(
-#             (self.wins.get((player, str(S)), 0) / 
-#              self.plays.get((player, str(S)), 1),
-#              self.plays.get((player, str(S)), 0),
-#              S)
-#             for S in moves_states
-#         )
-        
-        move = max(moves_states, key=lambda S: (self.wins.get((player, str(S)), 0) / self.plays.get((player, str(S)), 1), self.plays.get((player, str(S)), 0)))
-
+        move, _ = self.get_best(board, moves, who)
         return move
-    
-    
 
-    def sim(self, board, who):
-        plays, wins = self.plays, self.wins
 
+    def sim(self, board):
         visited_path = []
-#         states_copy = self.states[:]
-        state = board  # states_copy[-1]
-        player = who  # board.current_player(state)
-
-        expand = True
+        state = board
         winner = Board.STONE_NOTHING
-        for t in range(1, self.max_moves + 1):
-#             plt.matshow(state.stones.reshape(-1, Board.BOARD_SIZE))
+        for _ in range(1, self.max_moves + 1):
             moves, player = Game.possible_moves(state)
-            
-            # legal = board.legal_plays(states_copy)
-#             moves_states = moves  # [(p, board.next_state(state, p)) for p in legal]
-
-            if all(plays.get((player, str(S))) for S in moves):
-                log_total = log(sum(plays[(player, str(S))] for S in moves))
-                state_new = max(moves, key=lambda S: wins[(player, str(S))] / plays[(player, str(S))] + self.C * sqrt(log_total / plays[(player, str(S))]))
-#                 value, state_new = max((wins[(player, str(S))] / plays[(player, str(S))] + self.C * sqrt(log_total / plays[(player, str(S))]), S) for S in moves)
-            else:
-                state_new = choice(moves)
-
+            state_new, state_new_val = self.get_best(state, moves, player)
+            visited_path.append((player, state, state_new, state_new_val))
             over, winner, _ = state_new.is_over(state)
-            
-            state = state_new
-
-#             states_copy.append(state)
-
-            # `player` here and below refers to the player
-            # who moved into that particular state.
-            if expand and (player, str(state)) not in plays:
-#                 expand = False
-                plays[(player, str(state))] = 0
-                wins[(player, str(state))] = 0
-                if t > self.max_depth:
-                    self.max_depth = t
-
-            visited_path.append((player, str(state)))
-
-
-#             player = Board.oppo(player)#board.current_player(state_new)
-#             winner = board.winner(states_copy)
             if over:
                 break
+            state = state_new
 
-        for player, state in visited_path:
-            if (player, state) not in plays:
-                continue
-            plays[(player, state)] += 1
+        self.total_sim += 1
+
+        ds = SupervisedDataSet(self.features_num, 2)
+        for player, state, new, val in visited_path:
+            plays = val[1] * self.total_sim + 1
+            wins = val[0] * self.total_sim
             if player == winner:
-                wins[(player, state)] += 1
-        
-#         print(len(plays), len(wins))        
+                wins += 1
+            ds.addSample(self.get_input_values(state, new, player), (wins, plays))
+        self.trainer.trainOnDataset(ds)
 
-    def update(self, state):
-        self.states.append(state)
-        
-    
-    
+
+    def get_best(self, state, moves, who):
+        outputs = []
+        for s in moves:
+            out = self.net.activate(self.get_input_values(state, s, who))
+            outputs.append(out)
+        a = np.array(outputs)
+        b = a[:, 0] / a[:, 1] + self.C * np.log(np.sum(a[:, 1])) / a[:, 1]
+        i = np.argmax(b)
+        return moves[i], a[i]
+
+
+    def get_input_values(self, board, new_board, who):
+        v = board.stones
+        sz = v.shape[0]
+        iv = np.zeros(self.features_num)
+        iv[0:sz] = (v == Board.STONE_BLACK).astype(int)
+        iv[sz:sz * 2] = (v == Board.STONE_WHITE).astype(int)
+        iv[sz * 2:sz * 3] = (new_board.stones != v).astype(int)
+        iv[-2] = 1 if who == Board.STONE_BLACK else 0  # turn to black move
+        iv[-1] = 1 if who == Board.STONE_WHITE else 0  # turn to white move
+        return iv
+
