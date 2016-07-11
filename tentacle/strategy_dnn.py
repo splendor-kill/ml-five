a1f0446a776a847d84d1c07abe1bc7f952d7711b
splendor <splendor.kill@gmail.com>
2016-07-11 22:10:05 +0800

    integrate gomoku framework

--- tentacle/strategy_dnn.py
+++ tentacle/strategy_dnn.py
@@ -1,44 +1,55 @@
+import random
+
 import numpy as np
 from tentacle.board import Board
 from tentacle.dnn import Pre
 from tentacle.strategy import Strategy
 
-
-class StrategyANN(Strategy):
-
-    def __init__(self, features_num, hidden_neurons_num):
+class StrategyDNN(Strategy):
+    def __init__(self):
         super().__init__()
-        self.brain = Pre()
-        
+        self.brain = Pre(is_train=False, is_revive=True)
+        self.brain.run()
+
     def update_at_end(self, old, new):
         if not self.needs_update():
             return
-        
+
     def update(self, old, new):
-        if not self.needs_update():
-            return
+        pass
 
     def _update_impl(self, old, new, reward):
-        old_input = self.get_input_values(old)
-        
+        pass
+
     def board_value(self, board, context):
         pass
-        
+
     def preferred_board(self, old, moves, context):
         if not moves:
             return old
-        
-        iv = self.get_input_values(old)
-        
-        
-        
-        
+
+        v = old.stones
+
+        state = self.get_input_values(v)
+        best_move = self.brain.get_best_move(state)
+        try:
+            if v[best_move] == Board.STONE_EMPTY:
+                print(divmod(best_move, Board.BOARD_SIZE))
+                for m in moves:
+                    if moves[best_move] != Board.STONE_EMPTY:
+                        return m
+                raise Exception('impossible')
+            else:
+                raise Exception('invalid prediction')
+        except Exception:
+            return random.choice(moves)
+
     def get_input_values(self, board):
         black = (board == Board.STONE_BLACK).astype(float)
         white = (board == Board.STONE_WHITE).astype(float)
         valid = (board == Board.STONE_EMPTY).astype(float)
         return np.dstack((black, white, valid)).flatten()
-    
+
     def save(self, file):
         pass
 
@@ -47,6 +58,6 @@ class StrategyANN(Strategy):
 
     def setup(self):
         pass
-    
+
     def mind_clone(self):
-        pass    
+        pass
