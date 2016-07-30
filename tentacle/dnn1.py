318fa42b670ec504292a82856e88d8101ffff5f3
splendor <splendor.kill@gmail.com>
2016-07-30 14:51:53 +0800

    visualize mid-layers

--- tentacle/dnn1.py
+++ tentacle/dnn1.py
@@ -38,15 +38,15 @@ class DCNN1(Pre):
 #         W_22 = self.weight_variable([3, 1, ch, ch])
 #         b_22 = self.bias_variable([ch])
 
-        h_conv1 = tf.nn.relu(tf.nn.conv2d(states_pl, W_1, [1, 1, 1, 1], padding='VALID') + b_1)
-        h_conv2 = tf.nn.relu(tf.nn.conv2d(h_conv1, W_2, [1, 1, 1, 1], padding='SAME') + b_2)
-        h_conv21 = tf.nn.relu(tf.nn.conv2d(h_conv2, W_21, [1, 1, 1, 1], padding='SAME') + b_21)
+        self.h_conv1 = tf.nn.relu(tf.nn.conv2d(states_pl, W_1, [1, 1, 1, 1], padding='VALID') + b_1)
+        self.h_conv2 = tf.nn.relu(tf.nn.conv2d(self.h_conv1, W_2, [1, 1, 1, 1], padding='SAME') + b_2)
+        self.h_conv21 = tf.nn.relu(tf.nn.conv2d(self.h_conv2, W_21, [1, 1, 1, 1], padding='SAME') + b_21)
 #         h_conv22 = tf.nn.relu(tf.nn.conv2d(h_conv21, W_22, [1, 1, 1, 1], padding='SAME') + b_22)
 
-        shape = h_conv21.get_shape().as_list()
+        shape = self.h_conv21.get_shape().as_list()
         print(shape)
         dim = np.cumprod(shape[1:])[-1]
-        h_conv_out = tf.reshape(h_conv21, [-1, dim])
+        h_conv_out = tf.reshape(self.h_conv21, [-1, dim])
 
         num_hidden = 64
         W_3 = self.weight_variable([dim, num_hidden])
@@ -54,8 +54,8 @@ class DCNN1(Pre):
         W_4 = self.weight_variable([num_hidden, Pre.NUM_ACTIONS])
         b_4 = self.bias_variable([Pre.NUM_ACTIONS])
 
-        hidden = tf.nn.softplus(tf.matmul(h_conv_out, W_3) + b_3)
-        predictions = tf.matmul(hidden, W_4) + b_4
+        self.hidden = tf.nn.softplus(tf.matmul(h_conv_out, W_3) + b_3)
+        predictions = tf.matmul(self.hidden, W_4) + b_4
 
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(predictions, actions_pl)
         self.loss = tf.reduce_mean(cross_entropy)
@@ -77,7 +77,11 @@ class DCNN1(Pre):
         height = 6 * Board.BOARD_SIZE - 18  # row vecs + col vecs + valid(len>=5) main diag vecs + valid(len>=5) anti diag vecs
         return height, Board.BOARD_SIZE, Pre.NUM_CHANNELS
 
+    def mid_vis(self, feed_dict):
+        conv1, conv2, conv21, hide, pred = self.sess.run([self.h_conv1, self.h_conv2, self.h_conv21, self.hidden, self.predict_probs], feed_dict=feed_dict)
+        np.savez(Pre.MID_VIS_FILE, feed=feed_dict[self.states_pl], conv1=conv1, conv2=conv2, conv21=conv21, hide=hide, pred=pred)
+
 
 if __name__ == '__main__':
-    n1 = DCNN1()
+    n1 = DCNN1(is_revive=True)
     n1.run()
